566c5812a77f8d6d53d3183514111abeb5f1610c
Ou Ye <ou.ye.canada@hotmail.com>
2013-05-14 10:23:36 -0400

    Trying to add remote

--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -1,6 +1,7 @@
 jQuery.ajaxSetup({ 
 	'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 });
+
 $(document).ready(function() {
 	$("#new_comment").submit(function() {
 		$.post(this.action, $(this).serialize(), null, "script");
